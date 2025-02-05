a964b6843167e590bd540153af9834718e26fdac
jyh <dudgns3681@gmail.com>
2025-02-05 18:01:35 +0900

    Initial Commit

---
+++ src/main/java/com/jyhun/LiveStream/LiveStreamApplication.java
@@ -0,0 +1,13 @@
+package com.jyhun.LiveStream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveStreamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyhun/LiveStream/LiveStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyhun.LiveStream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveStreamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
