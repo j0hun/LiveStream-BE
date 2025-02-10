ff3109ac5d94831c015920716cf67bc862483744
jyh <dudgns3681@gmail.com>
2025-02-10 20:44:25 +0900

    FEAT: RoomRepository 추가

---
+++ src/main/java/com/jyhun/LiveStream/repository/RoomRepository.java
@@ -0,0 +1,7 @@
+package com.jyhun.LiveStream.repository;
+
+import com.jyhun.LiveStream.entity.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room,Long> {
+}
