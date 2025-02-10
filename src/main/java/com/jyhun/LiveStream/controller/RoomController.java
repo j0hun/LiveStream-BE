e38c6a47425acb20e8a87db8d22684baba5458fd
jyh <dudgns3681@gmail.com>
2025-02-10 21:11:13 +0900

    FEAT: Room 생성 기능 추가

--- src/main/java/com/jyhun/LiveStream/controller/RoomController.java
+++ src/main/java/com/jyhun/LiveStream/controller/RoomController.java
@@ -2,12 +2,22 @@ package com.jyhun.LiveStream.controller;
 
 import com.jyhun.LiveStream.service.RoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/room")
 public class RoomController {
 
     private final RoomService roomService;
+
+    @PostMapping
+    public ResponseEntity<Long> createRoom() {
+        return new ResponseEntity<>(roomService.createRoom(), HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/com/jyhun/LiveStream/service/RoomService.java
+++ src/main/java/com/jyhun/LiveStream/service/RoomService.java
@@ -1,5 +1,6 @@
 package com.jyhun.LiveStream.service;
 
+import com.jyhun.LiveStream.entity.Room;
 import com.jyhun.LiveStream.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -11,5 +12,12 @@ import org.springframework.transaction.annotation.Transactional;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+
+    public Long createRoom() {
+        Room room = Room.builder()
+                .build();
+        roomRepository.save(room);
+        return room.getId();
+    }
 
 }
