2c5c0a37bb051a5b62a207ac6f4076bef62505da
jyh <dudgns3681@gmail.com>
2025-02-11 20:11:01 +0900

    FEAT: 회원가입 기능 개발

--- src/main/java/com/jyhun/LiveStream/controller/UserController.java
+++ src/main/java/com/jyhun/LiveStream/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.jyhun.LiveStream.controller;
 
+import com.jyhun.LiveStream.dto.RegisterDTO;
 import com.jyhun.LiveStream.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Void> createUser(@RequestBody RegisterDTO registerDTO) {
+        return ResponseEntity.ok(userService.register(registerDTO));
+    }
 
 }
--- src/main/java/com/jyhun/LiveStream/service/UserService.java
+++ src/main/java/com/jyhun/LiveStream/service/UserService.java
@@ -1,7 +1,11 @@
 package com.jyhun.LiveStream.service;
 
+import com.jyhun.LiveStream.dto.RegisterDTO;
+import com.jyhun.LiveStream.entity.User;
+import com.jyhun.LiveStream.enums.Role;
 import com.jyhun.LiveStream.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,5 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public Void register(RegisterDTO registerDTO) {
+        User user = User.builder()
+                .username(registerDTO.getUsername())
+                .email(registerDTO.getEmail())
+                .password(passwordEncoder.encode(registerDTO.getPassword()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+
+        return null;
+    }
 
 }
