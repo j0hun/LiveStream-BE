790cbf7aee76216f2336d78e11dbda2e805683ac
jyh <dudgns3681@gmail.com>
2025-02-11 17:30:45 +0900

    FEAT: User Service 추가

---
+++ src/main/java/com/jyhun/LiveStream/service/UserService.java
@@ -0,0 +1,15 @@
+package com.jyhun.LiveStream.service;
+
+import com.jyhun.LiveStream.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+}
