cf352f88dd3f0fe66b42733554c9a2b83dfda398
jyh <dudgns3681@gmail.com>
2025-02-11 17:33:27 +0900

    FEAT: User Controller 추가

---
+++ src/main/java/com/jyhun/LiveStream/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.jyhun.LiveStream.controller;
+
+import com.jyhun.LiveStream.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+}
